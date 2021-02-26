56721360140042a458367c99df32c268825e5563
Natali202017 <75806805+Natali202017@users.noreply.github.com>
2021-02-26 22:55:28 +0500

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,33 @@
+# Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print('calculator')
+print("9+10 =")
+print(9+10)   # addition(+)
+print('3.6-0.6 = ')
+print(3.6-0.6) # subtraction(-)
+print('2*2 = ')   
+print(2*2)   # multiplication(*)
+print('8/4 = ')  
+print(8/4)   # division(/)
+print('2 ** 8 = ')  
+print(2 ** 8)  # exponential(**)
+print('7%3')
+print(7%3)   # modulus(%)
+print('9//4 = ')
+print(9//4) # Floor division operator(//)
+
+# Checking data types
+
+print(type(33))                  # Int
+print(type(3.99))                # Float
+print(type(3j+2))              # Complex
+print(type('Project'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.0, 9.64, 7.9}))    # Tuple
+
+#Print text
+
+print('I enjoy programming')
+print('It is interesting')
